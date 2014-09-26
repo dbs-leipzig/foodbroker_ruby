14a86f64710b236707c89eb3e9eca2b7f73f59fe
peet <petermann@informatik.uni-leipzig.de>
2014-09-26 12:30:57 +0200

    fixed missing entries in adjacency lists

--- store/food_broker_2_adjacency_list.rb
+++ store/food_broker_2_adjacency_list.rb
@@ -12,23 +12,26 @@ class FoodBroker2AdjacencyList
   end  
   
   def record objects
-    objects.each do |object|
-      if object.class.to_s.include? 'Line'
-        domain_objects = []
-        object.properties(false).each {|c,v|domain_objects.push v if v.kind_of? DomainData}
-        domain_objects.first.vids.push domain_objects.last.vid        
-        domain_objects.last.vids.push domain_objects.first.vid 
-      else
-        object.properties(false).each do |column,value|
-          if value.kind_of? DomainData
-            object.vids.push value.vid
-            value.vids.push object.vid
-          end
-        end
+    lines = objects.select {|o| o.class.to_s.include? 'Line'}
+    objects -= lines
 
-        dump.puts "#{object.vid},#{(object.kind_of? MasterData) ? 1 : 0},#{object.vids.uniq.join ' '}"
+    lines.each do |line|
+      domain_objects = []
+      line.properties(false).each {|c,v|domain_objects.push v if v.kind_of? DomainData}
+      domain_objects.first.vids.push domain_objects.last.vid        
+      domain_objects.last.vids.push domain_objects.first.vid 
+    end
+
+    objects.each do |object|
+      object.properties(false).each do |column,value|
+        if value.kind_of? DomainData
+          object.vids.push value.vid
+          value.vids.push object.vid
+        end
       end
     end
+
+    objects.each {|o| dump.puts "#{o.vid},#{(o.kind_of? MasterData) ? 1 : 0},#{o.vids.uniq.join ' '}"}
   end
   
   def finish!
